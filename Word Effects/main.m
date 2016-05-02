b8a7b381a94432d73dc34c2c9136658ed436dccd
RKing <rosalyn.kingsmill@gmail.com>
2016-05-02 17:12:34 -0400

    Add starting code

--- Word Effects/main.m
+++ Word Effects/main.m
@@ -10,8 +10,26 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+  
+        // 255 unit long array of characters
+        char inputChars[255];
+        
+        printf("Input a string: ");
+        
+        //limit input to max 255 characters
+        fgets(inputChars, 255, stdin);
+        
+        //print as a c string
+        printf("Your string is %s\n", inputChars);
+        
+        //convert char array to an NSString object
+        NSString *inputString = [NSString stringWithUTF8String:inputChars];
+        
+        //print NSString object
+        NSLog (@"Input was: %@", inputString);
+        
+        
+  
     }
     return 0;
 }
