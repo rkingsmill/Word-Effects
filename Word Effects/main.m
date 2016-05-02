13e425ea2832d0dd903cf151e64b38005a8b9faf
RKing <rosalyn.kingsmill@gmail.com>
2016-05-02 19:27:09 -0400

    Adding loop and if else statements

--- Word Effects/main.m
+++ Word Effects/main.m
@@ -10,9 +10,13 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-  
+        
+        //repeat loop
+        while (true) {
+            
         // 255 unit long array of characters
         char inputChars[255];
+            char inputNum[1];
         
         printf("Input a string: ");
         
@@ -28,38 +32,70 @@ int main(int argc, const char * argv[]) {
         //print NSString object
         NSLog (@"Input was: %@", inputString);
         
+        //ask what they would like to do to their string
+        printf("Choose from the following list:\n1. Change to uppercase\n2. Change to lowercase\n3. Change to an integer\n4. Make Canadian\n5. Get a response\n6. Replace spaces\n");
+            
+        //get input
+        fgets(inputNum, 6, stdin);
+        
+        //Convert to NSString
+        NSString *inputChoice = [NSString stringWithUTF8String:inputNum];
+            
         //Uppercase
+            if ([inputChoice isEqual: @"1"]){
+            
         NSString *upperCased = [inputString uppercaseString];
         //print uppercase
         NSLog (@"Yelling: %@", upperCased);
-  
+                
+            }
+            
         //Lowercase
+            if ([inputChoice isEqual: @"2"]){
         NSString *lowerCased = [inputString lowercaseString];
         //print lowercase
         NSLog (@"Lowercase: %@", lowerCased);
-        
+            }
         
         // change to integer
+            if ([inputChoice isEqual: @"3"]){
         int x = [inputString intValue];
         NSLog (@"Value: %d", x);
     
-       
+            }
+            
         //make canadian
+            if ([inputChoice isEqual: @"4"]) {
         NSString *canadian = @"";
         canadian =[inputString stringByAppendingString:@"eh"];
         //print canadian
         NSLog (@"Canadian: %@", canadian);
-        
+            }
+            
         //respond
+            if ([inputChoice isEqual: @"5"]) {
+        NSString *lastChar = [inputString substringFromIndex:[inputString length] - 1];
+        
+        if ([lastChar isEqual: @"!"]) {
+            
+            NSLog (@"Woah, calm down");
+        }
+        
+        else if ([lastChar isEqual:@"?"]){
+            NSLog (@"I don't know");
+        }
+            
+            }
         
         //change spaces
+            if ([inputChoice isEqual: @"6"]) {
         NSString *spaces = [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
         //print spaces
         NSLog (@"No spaces: %@", spaces);
-
+            }
       
 
         
-    }
+        } }
     return 0;
 }
